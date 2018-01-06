09d88fcd16b338facb3669a9d34b03ee988b5316
Yana Agun Siswanto <yana.developer@gmail.com>
2018-01-06 13:03:26 +0700

    Set "Others" to 0 to easily add other recognized  chat system without breaking compatibility.

--- resources/chat-types.js
+++ resources/chat-types.js
@@ -1,4 +1,5 @@
 module.exports = {
+  'OTHER': 0,
   'GITTER': 1,
   'SLACK': 2,
   'ZULIP': 3,
@@ -6,13 +7,12 @@ module.exports = {
   'ROCKET': 4,
   'TELEGRAM': 5,
   'IRC': 6,
-  'OTHER': 7,
 
+  0: 'OTHER',
   1: 'GITTER',
   2: 'SLACK',
   3: 'ZULIP',
   4: 'ROCKET',
   5: 'TELEGRAM',
   6: 'IRC',
-  7: 'OTHER',
 };
